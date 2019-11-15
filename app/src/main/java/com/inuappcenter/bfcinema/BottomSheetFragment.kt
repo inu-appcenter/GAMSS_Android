d2504055e3c8b9012fb158db629e110233478840
GowoonJ <jgw971229@naver.com>
2019-11-15 23:40:58 +0900

    bottom sheet 추가 marker 아이콘 변경

---
+++ app/src/main/java/com/inuappcenter/bfcinema/BottomSheetFragment.kt
@@ -0,0 +1,25 @@
+package com.inuappcenter.bfcinema
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+
+        return view
+    }
+}
--- app/src/main/java/com/inuappcenter/bfcinema/LocationMapActivity.kt
+++ app/src/main/java/com/inuappcenter/bfcinema/LocationMapActivity.kt
@@ -1,35 +1,59 @@
 package com.inuappcenter.bfcinema
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class LocationMapActivity : FragmentActivity() , OnMapReadyCallback {
+class LocationMapActivity : FragmentActivity() , OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        showBottomSheet()
+        return true
+    }
+
     lateinit var mMap : GoogleMap
 
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
         val seoul =
-            LatLng(37.52487, 126.92723)
+            LatLng(37.425434, 126.66528)
 
         // 구글 맵에 표시할 마커에 대한 옵션 설정
-        val makerOptions =
+        val markerOptions =
             MarkerOptions()
-        makerOptions
+        markerOptions
+//            .icon(BitmapDescriptorFactory.defaultMarker(R.drawable.map_marker))
             .position(seoul)
-            .title("원하는 위치(위도, 경도)에 마커를 표시했습니다.")
+            .title("청학동 베프시네마")
+
+
+        val bitmapDraw =
+            resources.getDrawable(R.drawable.map_marker) as BitmapDrawable
+        val b: Bitmap? = bitmapDraw.bitmap
+        val smallMarker: Bitmap? =
+            Bitmap.createScaledBitmap(b!!, dpToPx(baseContext,66), dpToPx(baseContext,83), false)
+        markerOptions.icon(
+            BitmapDescriptorFactory.fromBitmap(
+                smallMarker
+            )
+        )
 
         // 마커를 생성한다.
-        mMap.addMarker(makerOptions)
+        mMap.addMarker(markerOptions)
 
-        //카메라를 여의도 위치로 옮긴다.
         mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul))
+        mMap.setOnMarkerClickListener(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,5 +62,18 @@ class LocationMapActivity : FragmentActivity() , OnMapReadyCallback {
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
         mapFragment!!.getMapAsync(this)
+    }
+    fun showBottomSheet(){
+        val bottomSheetFragment=BottomSheetFragment()
+        bottomSheetFragment.show(supportFragmentManager,"bottomSheet")
+    }
+
+    // dp -> pixel 단위로 변경
+    private fun dpToPx(context: Context, dp: Int): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(),
+            context.resources.displayMetrics
+        ).toInt()
     }
 }
