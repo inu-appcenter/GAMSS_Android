abe610615ce2c5baedd92d41a2f79bb28e89cbb1
ChoiAhHyun <cah1113@naver.com>
2019-11-15 14:51:38 +0900

    MoviePager에서 영화 click event -> MovieDetail 처리 완료

--- app/src/main/java/com/inuappcenter/bfcinema/MovieDetailActivity.kt
+++ app/src/main/java/com/inuappcenter/bfcinema/MovieDetailActivity.kt
@@ -1,7 +1,12 @@
 package com.inuappcenter.bfcinema
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_movie_detail.*
+import kotlinx.android.synthetic.main.fragment_movie.iv_poster
+import kotlinx.android.synthetic.main.fragment_movie.tv_name
+import kotlinx.android.synthetic.main.layout_movie_detail.*
 
 class MovieDetailActivity : AppCompatActivity() {
 
@@ -9,5 +14,36 @@ class MovieDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
 
+        val movieName = intent.getStringExtra("movie")
+        val array: Array<String>
+        if (movieName.equals("movie1") ) {
+            iv_poster.setImageResource(R.drawable.this_week_movie1)
+            array = resources.getStringArray(R.array.movie1)
+            tv_name.text = array.get(0)
+            tv_genre.text = array.get(2)
+            tv_year.text = array.get(3)
+            tv_date.text = array.get(4)
+            tv_location.text = array.get(5)
+            tv_remaining.text = array.get(6)
+            tv_status.text = array.get(7)
+            tv_story.text = array.get(8)
+            tv_seats.text = array.get(9)
+            tv_runningtime.text = array.get(10)
+        } else if (movieName.equals("movie2")) {
+            iv_poster.setImageResource(R.drawable.this_week_movie2)
+            array = resources.getStringArray(R.array.movie2)
+            tv_name.text = array.get(0)
+            tv_genre.text = array.get(2)
+            tv_year.text = array.get(3)
+            tv_date.text = array.get(4)
+            tv_location.text = array.get(5)
+            tv_remaining.text = array.get(6)
+            tv_status.text = array.get(7)
+            tv_story.text = array.get(8)
+            tv_seats.text = array.get(9)
+            tv_runningtime.text = array.get(10)
+        } else {
+            Toast.makeText(this, "영화를 다시 선택해주세요", Toast.LENGTH_SHORT).show()
+        }
     }
 }
