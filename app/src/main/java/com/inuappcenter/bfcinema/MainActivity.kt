0564d1886cb67a7b7cde9885799bde8fc045784b
GowoonJ <jgw971229@naver.com>
2019-11-15 12:22:41 +0900

    투표 코드 추가

--- app/src/main/java/com/inuappcenter/bfcinema/MainActivity.kt
+++ app/src/main/java/com/inuappcenter/bfcinema/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.inuappcenter.bfcinema
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
@@ -30,6 +32,8 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         vp_movie.adapter = adapter
 
         btn_drawer.setOnClickListener(this)
+        linear_vote1.setOnClickListener(this)
+        linear_vote2.setOnClickListener(this)
     }
 
     override fun onBackPressed() {
@@ -40,12 +44,33 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
             super.onBackPressed()
         }
     }
+
     fun setProgressVote(select:Int){
         if (enabledVote){
             when (select){
-                VOTE_1 -> shadow_view_1.visibility = View.INVISIBLE
-                VOTE_2 -> shadow_view_2.visibility = View.INVISIBLE
+                VOTE_1 -> {
+                    shadow_view_1.visibility = View.INVISIBLE
+                    progressBar.setProgress(24,true)
+                    tv_percent1.text = "24%"
+                    tv_percent2.text = "76%"
+                    progressBar.rotation = 180F
+
+                    tv_movie1.setTextColor(Color.parseColor("#3effde"))
+                    tv_movie_info1.setTextColor(Color.parseColor("#3effde"))
+                }
+                VOTE_2 -> {
+                    shadow_view_2.visibility = View.INVISIBLE
+                    progressBar.setProgress(82,true)
+                    tv_percent1.text = "18%"
+                    tv_percent2.text = "82%"
+
+                    tv_movie2.setTextColor(Color.parseColor("#3effde"))
+                    tv_movie_info2.setTextColor(Color.parseColor("#3effde"))
+                }
             }
+            progressBar.visibility = View.VISIBLE
+            tv_percent1.visibility = View.VISIBLE
+            tv_percent2.visibility = View.VISIBLE
             enabledVote = false
         }else{
             Toast.makeText(this,"투표는 한 번만 가능합니다",Toast.LENGTH_SHORT).show()
