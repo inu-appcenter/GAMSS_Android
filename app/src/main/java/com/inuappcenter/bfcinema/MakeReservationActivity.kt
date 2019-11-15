6cd69d81c321756f65bc635744a78e4e2bd119fa
ChoiAhHyun <cah1113@naver.com>
2019-11-15 18:49:31 +0900

    nice-spinner 라이브러리 추가, Spinner 완성

--- app/src/main/java/com/inuappcenter/bfcinema/MakeReservationActivity.kt
+++ app/src/main/java/com/inuappcenter/bfcinema/MakeReservationActivity.kt
@@ -2,6 +2,9 @@ package com.inuappcenter.bfcinema
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.angmarch.views.NiceSpinner
+import java.util.*
+
 
 class MakeReservationActivity  : AppCompatActivity() {
 
@@ -9,5 +12,8 @@ class MakeReservationActivity  : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_reservation)
 
+        val niceSpinner = findViewById<NiceSpinner>(R.id.spinner_people)
+        val dataset = LinkedList(listOf("1명", "2명"))
+        niceSpinner.attachDataSource<String>(dataset)
     }
 }
