a09b79189ce20d9938593beecd4ef83eda930cdc
GowoonJ <jgw971229@naver.com>
2019-11-15 22:25:29 +0900

    구글 맵 api 사용 진행 중

---
+++ app/src/main/java/com/inuappcenter/bfcinema/LocationMapActivity.kt
@@ -0,0 +1,12 @@
+package com.inuappcenter.bfcinema
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_map)
+    }
+}
