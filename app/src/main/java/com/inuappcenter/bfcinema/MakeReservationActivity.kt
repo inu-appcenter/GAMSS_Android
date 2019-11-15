1929f912286e571af64723c0469c9034c89ba2bd
ChoiAhHyun <cah1113@naver.com>
2019-11-15 15:29:37 +0900

    MakeReservationActivity 추가, MovieDetailActivity에 onclicklistener 적용

---
+++ app/src/main/java/com/inuappcenter/bfcinema/MakeReservationActivity.kt
@@ -0,0 +1,13 @@
+package com.inuappcenter.bfcinema
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MakeReservationActivity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_reservation)
+
+    }
+}
--- app/src/main/java/com/inuappcenter/bfcinema/MovieDetailActivity.kt
+++ app/src/main/java/com/inuappcenter/bfcinema/MovieDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.inuappcenter.bfcinema
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_movie_detail.*
@@ -8,42 +10,65 @@ import kotlinx.android.synthetic.main.fragment_movie.iv_poster
 import kotlinx.android.synthetic.main.fragment_movie.tv_name
 import kotlinx.android.synthetic.main.layout_movie_detail.*
 
-class MovieDetailActivity : AppCompatActivity() {
+class MovieDetailActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
 
         val movieName = intent.getStringExtra("movie")
+        choiceMovie(movieName)
+
+        btn_back.setOnClickListener(this)
+        btn_reservation.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_back -> {
+                onBackPressed()
+            }
+//            R.id.btn_bookmark -> {
+//            }
+            R.id.btn_reservation -> {
+                val intent = Intent(this, MakeReservationActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun choiceMovie(movieName:String) {
         val array: Array<String>
-        if (movieName.equals("movie1") ) {
-            iv_poster.setImageResource(R.drawable.this_week_movie1)
-            array = resources.getStringArray(R.array.movie1)
-            tv_name.text = array.get(0)
-            tv_genre.text = array.get(2)
-            tv_year.text = array.get(3)
-            tv_date.text = array.get(4)
-            tv_location.text = array.get(5)
-            tv_remaining.text = array.get(6)
-            tv_status.text = array.get(7)
-            tv_story.text = array.get(8)
-            tv_seats.text = array.get(9)
-            tv_runningtime.text = array.get(10)
-        } else if (movieName.equals("movie2")) {
-            iv_poster.setImageResource(R.drawable.this_week_movie2)
-            array = resources.getStringArray(R.array.movie2)
-            tv_name.text = array.get(0)
-            tv_genre.text = array.get(2)
-            tv_year.text = array.get(3)
-            tv_date.text = array.get(4)
-            tv_location.text = array.get(5)
-            tv_remaining.text = array.get(6)
-            tv_status.text = array.get(7)
-            tv_story.text = array.get(8)
-            tv_seats.text = array.get(9)
-            tv_runningtime.text = array.get(10)
-        } else {
-            Toast.makeText(this, "영화를 다시 선택해주세요", Toast.LENGTH_SHORT).show()
+        when (movieName) {
+            "movie1" -> {
+                iv_poster.setImageResource(R.drawable.this_week_movie1)
+                array = resources.getStringArray(R.array.movie1)
+                tv_name.text = array.get(0)
+                tv_genre.text = array.get(2)
+                tv_year.text = array.get(3)
+                tv_date.text = array.get(4)
+                tv_location.text = array.get(5)
+                tv_remaining.text = array.get(6)
+                tv_status.text = array.get(7)
+                tv_story.text = array.get(8)
+                tv_seats.text = array.get(9)
+                tv_runningtime.text = array.get(10)
+            }
+            "movie2" -> {
+                iv_poster.setImageResource(R.drawable.this_week_movie2)
+                array = resources.getStringArray(R.array.movie2)
+                tv_name.text = array.get(0)
+                tv_genre.text = array.get(2)
+                tv_year.text = array.get(3)
+                tv_date.text = array.get(4)
+                tv_location.text = array.get(5)
+                tv_remaining.text = array.get(6)
+                tv_status.text = array.get(7)
+                tv_story.text = array.get(8)
+                tv_seats.text = array.get(9)
+                tv_runningtime.text = array.get(10)
+            }
+            else -> Toast.makeText(this, "영화를 다시 선택해주세요", Toast.LENGTH_SHORT).show()
         }
     }
 }
